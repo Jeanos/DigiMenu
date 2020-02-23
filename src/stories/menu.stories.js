0efddf69a031c15c001a8a9d7d5101bd7b406531
jean-jacques Johnson <jeanjacques.johnson@gmail.com>
2020-02-23 06:28:15 -0800

    First DashBoard
    First Dashboard with dashboard comps

--- src/stories/menu.stories.js
+++ src/stories/menu.stories.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import MainHeader from '../comps/MainHeader';
-
-import {action} from '@storybook/addon-actions';
+//import {action} from '@storybook/addon-actions';
 
 export default {
   title: 'MainHeader',
