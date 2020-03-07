d5e860f02c289f2f56a25ab8631b8671e01d4b2a
AdamsonA <adamsonlion@gmail.com>
2020-03-06 20:18:18 -0800

    Review comp

---
+++ src/comps/Review.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { FaBeer } from 'react-icons/fa';
+import { MdAccountCircle } from "react-icons/md";
+import '../css/Review.css'
+import StarRatings from 'react-star-ratings';
+
+
+function Review (){
+    return(
+        <div id="main">
+            <hr/>
+        < div id="name">
+          <MdAccountCircle/>  
+                <text id="first">David Wand</text>
+        </div>
+        <StarRatings
+          starRatedColor="blue"
+          numberOfStars={6}
+          name='rating'
+          starDimension="15px"
+        starSpacing="2px"
+        /> <text id="startxt">A week ago...</text>
+        <div id="desc">
+            <text>The Food Was Amazing, it was filled well and the prawns where fresh</text>
+        </div>
+        </div>
+        
+    )
+}
+
+export default Review;
